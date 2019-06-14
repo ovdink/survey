5c4722781879a52c2714c7fb79a7450d8df22eb2
henker <ovdink@gmail.com>
2019-06-14 12:57:56 +0300

    alpha 0.2v

--- scripts.js
+++ scripts.js
@@ -4,48 +4,60 @@ let allValues = [];
 
 function addName(value) {
   count++;
-
+  var op = document.getElementById('select').getElementsByTagName("option");
+  for (var i = 0; i < op.length; i++) {
+  if (op[i].value == value) {
+    op[i].disabled = true;
+  }
+}
   let newElement = document.createElement('div');
   let newValue = document.createElement('input');
+  let newComment = document.createElement('input')
+  let newDelete = document.createElement('img');
+
+  newDelete.setAttribute('src', 'https://img.icons8.com/ios/26/000000/delete-sign.png');
   newValue.setAttribute('type', 'number');
-  console.log('new value', newValue);
+
   newValue.onchange = (e) => {
     let wasThereANumber = allValues[value] ? allValues[value] : 0;
-    if (Number(e.target.value) <= limitVal && Number(e.target.value) >= 0) {
+    if (Number(e.target.value) - 1 < limitVal && Number(e.target.value) >= 0) {
       allValues[value] = Number(e.target.value);
-      console.log('onchan', e.target.value);
       limit(allValues[value], wasThereANumber);
-      console.log(allValues);
     }
     else
       alert('Неверное значение!');
   };
-  let newComment = document.createElement('input')
+
+  
+  newElement.textContent = select.value;
+  surnames.appendChild(newElement);
+  surnames.appendChild(newValue);
+  surnames.appendChild(newComment);
+  surnames.appendChild(newDelete);
 
   newElement.setAttribute('id', 'windowSurname');
   newValue.setAttribute('id', 'windowValue');
   newComment.setAttribute('id', 'windowComment');
-
-  newElement.textContent = select.value;
-
-  surnames.appendChild(newElement);
-  surnames.appendChild(newValue);
-  surnames.appendChild(newComment);
+  newDelete.setAttribute('id', 'imgDelete');
 
   if (count > max) {
     alert("Превышен лимит!");
   }
+
+  //сделать нажатие не на поле а на newDelete
   newElement.addEventListener('click', delName);
-}
+};
 
 function limit(val, wasThereANumber) {
   limitVal = wasThereANumber === 0 ?  limitVal : (limitVal + wasThereANumber);
   limitVal = limitVal - val;
-
-  console.log('limit_after_change', limitVal);
   document.getElementById('limit').value = limitVal;
-}
+};
 
 function delName() {
-  windowSurname.parentElement.removeChild(windowSurname, windowComment, windowValue);
-}
+  //прибавить value удаленного человека к limitVal
+  //удалить всю строку при нажатии на крестик, включая поля.
+  //сделать enabled в select
+
+  //windowSurname.parentElement.removeChild(windowSurname, windowComment, windowValue);
+};
